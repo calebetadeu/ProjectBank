1aee39b3791bdd25ba8f7f7dcb260f4efd1d9b22
calebetadeu <calebetadeu@gmail.com>
2021-02-18 21:43:37 -0300

    Update to talk about class members

--- src/main/kotlin/Pessoa.kt
+++ src/main/kotlin/Pessoa.kt
@@ -2,12 +2,12 @@ class Pessoa {
     var nome:String="Calebe"
     var cpf:String="4552.544.6554-87"
     private set
+    constructor()
 
+    fun pessoainfo()="$nome, $cpf"
 }
 fun main(){
-    var calebe=Pessoa()
+    val calebe=Pessoa()
 
-    println(calebe.nome)
-    println(calebe.cpf)
-
+  println(calebe.pessoainfo())
 }
