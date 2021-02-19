645a5dd511f38d0d9bcdb97958b2e116ac9beb03
calebetadeu <calebetadeu@gmail.com>
2021-02-18 21:32:07 -0300

    Update to apply the private modifier to set function of cpf

--- src/main/kotlin/Pessoa.kt
+++ src/main/kotlin/Pessoa.kt
@@ -1,9 +1,13 @@
 class Pessoa {
     var nome:String="Calebe"
     var cpf:String="4552.544.6554-87"
+    private set
+
 }
 fun main(){
     var calebe=Pessoa()
+
     println(calebe.nome)
     println(calebe.cpf)
+
 }
