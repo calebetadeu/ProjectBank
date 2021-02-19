69d98bcfd736f7f4f113a61774a01f810e774ebf
calebetadeu <calebetadeu@gmail.com>
2021-02-18 22:18:31 -0300

    Update to created the account with primary constructor and the functions : deposit and withdraw

---
+++ src/main/kotlin/Tests/BancoTest.kt
@@ -0,0 +1,10 @@
+package Tests
+
+import bankdigital.brasil.example.Banco
+
+fun main (){
+    val digitalBank=Banco(nome="BankMeta",numero = 12)
+
+    println(digitalBank)
+
+}
---
+++ src/main/kotlin/bankdigital/brasil/example/Banco.kt
@@ -0,0 +1,6 @@
+package bankdigital.brasil.example
+
+data class Banco(
+    val nome:String,
+    val numero:Int
+)
