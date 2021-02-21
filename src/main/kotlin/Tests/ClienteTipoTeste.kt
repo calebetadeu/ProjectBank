3cf3f45ded3ee60b3faf2cb2bc65e48066f9d070
calebetadeu <calebetadeu@gmail.com>
2021-02-21 01:18:13 -0300

    create enum class client type and add it

---
+++ src/main/kotlin/Tests/ClienteTipoTeste.kt
@@ -0,0 +1,15 @@
+package Tests
+import bankdigital.brasil.example.conta.ClienteTipo
+
+fun main(){
+    ClienteTipo.values().forEach {elemento->
+        println("${elemento.name} - ${elemento.descricao}")
+    }
+    ClienteTipo.values().forEach {
+        println("${it.name} - ${it.descricao}")
+    }
+    val pf=ClienteTipo.pf
+    val pj=ClienteTipo.pj
+    println("${pf.name} >>>> ${pf.descricao}")
+    print("${pj.name} >>>>>> ${pj.descricao}")
+}
---
+++ src/main/kotlin/bankdigital/brasil/example/conta/ClienteTipo.kt
@@ -0,0 +1,6 @@
+package bankdigital.brasil.example.conta
+
+enum class ClienteTipo(val descricao: String) {
+    pj("conta corrente"),
+    pf("conta fisica")
+}
